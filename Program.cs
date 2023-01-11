7f46e5052ac55b989f07d2e36f8a6307b8af6474
Danilo Lima <danilo.lima.developer@gmail.com>
2023-01-11 14:00:05 -0300

    Desafio Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia myNokia = new Nokia(numero:"(81)9.9100-0000",modelo: "12c",imei: "24249129490124-1294",memoria: 32);
+myNokia.MostrarEspecificacoesSmartphone();
+myNokia.Ligar();
+myNokia.InstalarAplicativo("Dio");
+
+Console.WriteLine("\n");
+
+Iphone myIphone = new Iphone(numero:"(81)9.9200-0000",modelo: "1FX",imei: "12949129490124-1294",memoria: 64);
+myIphone.MostrarEspecificacoesSmartphone();
+myIphone.ReceberLigacao();
+myIphone.InstalarAplicativo("DuoLingo");
